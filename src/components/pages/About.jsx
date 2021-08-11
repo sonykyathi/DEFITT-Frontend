51b1030a1ff890255d26d7a3ba88a0e40c1b3ecc
AbdulRehman <ar.jameel096@gmail.com>
2021-08-11 15:03:57 +0500

    updated FTT link instead of coin on logo click

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,27 +1,27 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 
 const About = (props) => {
   return (
     <Fragment>
-      <section className='about section-padding' id='about'>
-        <div className='container'>
-          <div className='heading text-center'>
+      <section className="about section-padding" id="about">
+        <div className="container">
+          <div className="heading text-center">
             <div
-              className='animated'
-              data-animation='fadeInUpShorter'
-              data-animation-delay='0.3s'
+              className="animated"
+              data-animation="fadeInUpShorter"
+              data-animation-delay="0.3s"
             >
-              <h6 className='sub-title'>About Us</h6>
-              <h2 className='title'>About Us</h2>
+              <h6 className="sub-title">About Us</h6>
+              <h2 className="title">About Us</h2>
             </div>
           </div>
-          <div className='content-area'>
-            <div className='row'>
-              <div className='col-md-12 col-lg-6' style={{ margin: 'auto 0' }}>
-                <ul className='app-features'>
-                  <li className='about-text dark-bg-text-color'>
-                    <p className=' white'>
+          <div className="content-area">
+            <div className="row">
+              <div className="col-md-12 col-lg-6" style={{ margin: "auto 0" }}>
+                <ul className="app-features">
+                  <li className="about-text dark-bg-text-color">
+                    <p className=" white">
                       DEFItt is a solution that allows you to enjoy success and
                       inspired experiences while also assisting you in becoming
                       venture financed with greater benefits and fewer risks.
@@ -34,35 +34,44 @@ const About = (props) => {
                       difficulties as well as pressures. DEFItt is using the BEP
                       20 Platform on the Binance Smart Chain which enables the
                       transactions security, speed and reliability.
-                    </p>{' '}
-                    <p className=' white'>
+                    </p>{" "}
+                    <p className=" white">
                       Blockchain is the future and we are all aware of It
                       however the right usage of blockchain is what makes DEFItt
                       different from all. An ecosystem that DEFItt has made with
                       its paired stable token FTT, makes this token touch human
                       lives.
-                    </p>{' '}
-                    <p className=' white'>
+                    </p>{" "}
+                    <p className=" white">
                       DEFItt is paired by FTT which can give access to user to
                       our different verticals.
                     </p>
-                    <div className='about-btn'>
-                      <a className='btn btn-lg btn-gradient-purple btn-glow animated' data-animation='fadeInUpShorter' href='/aboutus' target='_blank'>explore defitt</a>
+                    <div className="about-btn">
+                      <a
+                        className="btn btn-lg btn-gradient-purple btn-glow animated"
+                        data-animation="fadeInUpShorter"
+                        href="/aboutus"
+                        target="_blank"
+                      >
+                        explore defitt
+                      </a>
                     </div>
                   </li>
                 </ul>
               </div>
               <div
-                className='col-md-12 col-lg-6 animated'
-                data-animation='fadeInUpShorter'
-                data-animation-delay='0.7s'
+                className="col-md-12 col-lg-6 animated"
+                data-animation="fadeInUpShorter"
+                data-animation-delay="0.7s"
               >
-                <div className='what-is-crypto-img'>
-                  <img
-                    className='img-fluid'
-                    src='theme-assets/images/what-is-crypto.png'
-                    alt='What is Crypto?'
-                  />
+                <div className="what-is-crypto-img">
+                  <a href="https://ftttoken.com" target="_blank">
+                    <img
+                      className="img-fluid"
+                      src="theme-assets/images/what-is-crypto.png"
+                      alt="What is Crypto?"
+                    />
+                  </a>
                   {/* <div className='play-video text-center'>
                     <img src='theme-assets/images/hero-coin.png' width='100' />
                   </div> */}
