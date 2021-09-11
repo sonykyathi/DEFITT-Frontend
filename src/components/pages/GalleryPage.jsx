0b1ddef94a28014c31b7e160643512a2a48d5219
SaqibAltafTech <tech.lover152@gmail.com>
2021-09-11 22:54:37 +0500

    some post from galary removed

--- src/components/pages/GalleryPage.jsx
+++ src/components/pages/GalleryPage.jsx
@@ -1,7 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 const Gallery = (props) => {
+  useEffect(() => {
+    var options = {
+      method: 'GET',
+      url: 'https://axesso-facebook-data-service.p.rapidapi.com/fba/facebook-lookup-posts',
+      params: {url: 'https://www.facebook.com/longhornsteakhouse/posts/'},
+      headers: {
+        'x-rapidapi-host': 'axesso-facebook-data-service.p.rapidapi.com',
+        'x-rapidapi-key': 'undefined'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, [])
   return (
     <Fragment>
       <section className='gallery section-padding' id='gallery'>
@@ -24,21 +42,13 @@ const Gallery = (props) => {
                           <img className="img-fluid gt-tag" src="https://qph.fs.quoracdn.net/main-qimg-4079c882451673a969ea7167d61bc7e1" alt=""/>
                       </a>
                   </div>
-                  <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 wow fadeInUp" data-wow-delay="0.10s">
-                      <a href="javascript:void(0);" className="gallery-thumbnail-card">
-                          <img className="img-fluid gt-tag" src="https://source.unsplash.com/aob0ukAYfuI/400x300" alt=""/>
-                      </a>
-                  </div>
+                 
                   <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 wow fadeInUp" data-wow-delay="0.10s">
                       <a href="javascript:void(0);" className="gallery-thumbnail-card">
                           <iframe className="gt-tag" src="https://www.youtube.com/embed/Y94LNw5SUbM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
                       </a>
                   </div>
-                  <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 wow fadeInUp" data-wow-delay="0.10s">
-                      <a href="javascript:void(0);" className="gallery-thumbnail-card">
-                          <img className="img-fluid gt-tag" src="https://source.unsplash.com/EUfxH-pze7s/400x300" alt=""/>
-                      </a>
-                  </div>
+                 
                   <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 wow fadeInUp" data-wow-delay="0.10s">
                       <a href="javascript:void(0);" className="gallery-thumbnail-card">
                           <iframe className="gt-tag" src="https://www.youtube.com/embed/LPyxG2uKGbk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
@@ -49,11 +59,7 @@ const Gallery = (props) => {
                           <iframe className="gt-tag" src="https://www.youtube.com/embed/SGkwNe6PHhM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
                       </a>
                   </div>
-                  <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 wow fadeInUp" data-wow-delay="0.10s">
-                      <a href="javascript:void(0);" className="gallery-thumbnail-card">
-                          <img className="img-fluid gt-tag" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt=""/>
-                      </a>
-                  </div>
+                 
               </div>
           </div>
           </div>
