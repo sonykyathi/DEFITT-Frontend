dfc6280f74d6ef230e9d249a385af6d94d6ca620
adeelrana45 <adeel.rana.designer@gmail.com>
2021-09-15 17:41:09 +0500

    about us page

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -106,7 +106,7 @@ const AboutPage = (props) => {
                   />
                 </div>
                 <h4>Global Market Overview</h4>
-                <p className='white mb-2'>
+                <p className='white mb-4'>
                   In 2021, cryptocurrency has its finest year ever. While
                   virtual currencies were one of the few businesses to thrive in
                   2020, they are just getting more widespread, popular, and
@@ -145,31 +145,16 @@ const AboutPage = (props) => {
                   the highest appreciation of cryptocurrency’s in history and it
                   is becoming a part of the industry that is here to stay.
                 </p>
-                <p className='white mb-2'>
-                  Essential features and benefits are as follows: 1) The company
-                  that issues and redeems Financial Tech Token- FTT is FIT
-                  Technology Limited UK. FTT is issued directly by FIT
-                  TECHNOLOGIES LIMITED UK as a means of confidence-building,
-                  removing the need for an intermediary. This allows for quicker
-                  redemption times and exchanges for dollars at cheaper rates
-                  within one business day. 2) FTT can be sent or received by
-                  anybody who has a Binance wallet. On the Binance platform, all
-                  transactions adhere to the BEP-20 protocol and are regulated
-                  by smart contract regulations. As a result of this, smart
-                  contract transactions are free of human error, and the system
-                  only works as intended. 3) FTT is accessible 24 hours a day, 7
-                  days a week to assist settlement against any sort of asset,
-                  these include crypto, security, and asset tokens, as well as
-                  payments. Unlike fiat, which can only be used to settle deals
-                  during bank business hours, FTT may be transferred anywhere,
-                  at any time. 4) FTT is accessible for listing in various
-                  exchanges. Being backed by the dollar, FTT may be used as an
-                  alternative for assessing the dollars' worth in comparison to
-                  other digital assets. 5) FTT is a token built on the Binance
-                  Smart chain that can participate in the wider global community
-                  of tokens, assisting in the creation of a worldwide platform
-                  for programmable money with stability.
-                </p>
+                <div className='about-points'>
+                  <span>Essential features and benefits are as follows:</span>
+                  <ul>
+                    <li>1) The company that issues and redeems Financial Tech Token- FTT is FIT TechnologyLimited UK. FTT is issued directly byFIT TECHNOLOGIES LIMITED UK as a means of confidence-building,removing the need for an intermediary. This allows for quicker redemption times and exchanges for dollars at cheaper rates within one business day.</li>
+                    <li>2) FTT can be sent or received by anybodywho has a Binance wallet. On the Binance platform, all transactionsadhere to the BEP-20 protocol and are regulated bysmart contract regulations. As a result of this,smart contract transactions are free of human error,and the system only works as intended.</li>
+                    <li>3) FTT is accessible 24 hours a day,7 days a week to assist settlement against any sort of asset, these include crypto, security,and asset tokens, as well as payments. Unlike fiat,which can only be used to settle deals during bank business hours, FTT may be transferred anywhere,at any time.</li>
+                    <li>4) FTT is accessible for listing in various exchanges. Being backed by the dollar,FTT may be used as an alternative for assessingthe dollars'worth in comparison to other digital assets.</li>
+                    <li>5) FTT is a token built on the Binance Smart chain that can participate in the wider global community of tokens, assisting in the creation of a worldwide platform for programmable money with stability." should be in point wise. </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
